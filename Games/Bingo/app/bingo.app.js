9a4f7b8d9cfcc5416bf07a5279a93e306c8c8b46
soumya2080 <soumya2080@gmail.com>
2016-09-03 19:04:06 +0530

    Fixed Login and Signup css for mobile devices.

--- Games/Bingo/app/bingo.app.js
+++ Games/Bingo/app/bingo.app.js
@@ -1,9 +1,10 @@
-(function(){
+(function() {
     'use strict';
 
     angular
         .module("BingoApp", [
             'ngMaterial',
+            'ngMdIcons',
             'ui.router',
             'oc.lazyLoad',
             'AngularSocket'
@@ -11,14 +12,16 @@
         .run(function($rootScope, $state, AuthService) {
             $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
                 //console.log("fromState: "+angular.toJson(fromState)+", toSate:"+angular.toJson(toState));
-                if(toState.name !== 'login' && toState.name !== 'signup') {
+                if (toState.name !== 'login' && toState.name !== 'signup') {
                     event.preventDefault();
                     if (AuthService.isLoggedIn() === false) {
-                        $state.go('login', {toState: toState});
+                        $state.go('login', {
+                            toState: toState
+                        });
                     }
-                } 
-                
+                }
+
             });
-        });      
+        });
 
 })()
